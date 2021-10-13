5b9f8bf617055c32c9cbf4967d4184bcd904c13b
jiyeme <jiyecafe@gmail.com>
2021-10-13 12:51:33 +0800

    feat(robot): 新闻提醒计划任务

--- src/main/java/cn/wecuit/backen/WeCuitTask.java
+++ src/main/java/cn/wecuit/backen/WeCuitTask.java
@@ -1,11 +1,15 @@
 package cn.wecuit.backen;
 
 import cn.wecuit.backen.services.NewsService;
+import cn.wecuit.robot.RobotMain;
+import cn.wecuit.robot.data.NewsStorage;
+import cn.wecuit.robot.plugins.msg.NewsPlugin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  * @Author jiyec
@@ -18,8 +22,21 @@ public class WeCuitTask {
     @Resource
     NewsService newsService;
 
+    // 从第0分钟开始每隔10分钟执行一次
     @Scheduled(cron = "1 0/10 * * * ?")
-    public void newsTask(){
+    public void newsPullTask(){
         newsService.pullNews();
+    }
+    // 从第5分钟开始每隔10分钟执行一次
+    @Scheduled(cron = "0 5/10 * * * ?")
+    public void newsNoticeTask(){
+        NewsStorage.delOutDate();
+        if(RobotMain.getBot() != null){
+            try {
+                newsService.newsNotice(NewsPlugin.getEnabledList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
